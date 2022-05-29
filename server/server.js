31ed29afbc49c5645812af5f3f9ecfefb43816ed
patwicks <it.patrickjohngsantillan@gmail.com>
2022-05-29 13:36:25 +0800

    add files server technote-dev

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://technote-dev.netlify.app",
     credentials: true,
   })
 );
